d567284a3a9769df83ba562af20ad55f6ce25789
bhadraBytes <yashTechMail@proton.me>
2024-01-03 01:59:55 +0530

    Major changes in navbar and herobanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,36 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { urlFor } from '../lib/client';
-import styles from '../styles/Home.module.css'; // Import your CSS file
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
+import styles from "../styles/Home.module.css"; // Import your CSS file
 
 const HeroBanner = ({ heroBanner }) => {
-  const [backgroundColor, setBackgroundColor] = useState('#ffff');
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setBackgroundColor(getRandomColor());
-    }, 10000);
-
-    // Cleanup the interval on component unmount
-    return () => clearInterval(intervalId);
-  }, []);
-
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
   return (
-    <div className={`hero-banner-container ${styles.heroBanner}`} style={{ backgroundColor }}>
+    <div className={`hero-banner-container ${styles.heroBanner}`}>
       <div>
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
 
         <div>
           <Link href={`/product/${heroBanner.product}`}>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
 import { TbSearch } from "react-icons/tb";
+import { FaRegHeart } from "react-icons/fa";
+import { FiUser } from "react-icons/fi";
 import { Cart } from "./";
 import { useStateContext } from "../context/StateContext";
 
@@ -11,33 +13,54 @@ const Navbar = () => {
   return (
     <>
       <div className="anouncement-container">
-        <p className="p-text">Sign up and get 20% off to your first order. Sign Up Now</p>
-        <p className="p-text">Sign up and get 20% off to your first order. Sign Up Now</p>
-        <p className="p-text">Sign up and get 20% off to your first order. Sign Up Now</p>
+        <p className="p-text">
+          Sign up and get 20% off to your first order. Sign Up Now
+        </p>
+        <p className="p-text">
+          Sign up and get 20% off to your first order. Sign Up Now
+        </p>
+        <p className="p-text">
+          Sign up and get 20% off to your first order. Sign Up Now
+        </p>
+      </div>
+      <div className="logo">
+        <Link href="/">
+          <img src="./assets/bhadrabytes.png" alt="" />
+        </Link>
       </div>
       <div className="navbar-container">
         <div className="app__header">
           <ul className="left">
-            <li onClick={() => navigate("/")}>Home</li>
-            <li>About</li>
-            <li>Contact Us</li>
+            <div className="app__header-icon">
+              <h3 className="search">Search</h3><TbSearch className="search-icon"/>
+            </div>
+            <div className="app__header-icon user">
+              <FiUser />
+            </div>
           </ul>
-
-          <p className="logo">
-            <Link href="/">
-              <img src="./assets/bhadrabytes.png" alt="" />
-            </Link>
-          </p>
+          <ul className="center">
+            <li>Home</li>
+            <li>All Products</li>
+            <li>All Products</li>
+            <li>All Products</li>
+            <li>New Arrivals</li>
+            <li>Tops</li>
+            <li>Bottoms</li>
+            <li>Dresses</li>
+            <li>Sets</li>
+          </ul>
           <div className="right">
-            <TbSearch className="search-icon" />
-            <button
+              <div className="heart app__header-icon">
+                <FaRegHeart />
+              </div>
+            <div 
               type="button"
               className="cart-icon"
               onClick={() => setShowCart(true)}
             >
-              <AiOutlineShopping />
+              <h3 className="search">Cart</h3><AiOutlineShopping className="cart"/>
               <span className="cart-item-qty">{totalQuantities}</span>
-            </button>
+            </div>
 
             {showCart && <Cart />}
           </div>
